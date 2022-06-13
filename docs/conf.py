98a2fe3dd6da1f48d32aef3487521ccf005cc892
Edwin Yllanes <e.yllanescucho@gmail.com>
2022-06-13 13:24:19 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,3 +4,22 @@ author = "eyllanesc"
 extensions = [
     "myst_parser",
 ]
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+    "deflist",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "colon_fence",
+    "smartquotes",
+    "replacements",
+    "linkify",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+myst_number_code_blocks = ["typescript"]
+myst_heading_anchors = 5
+myst_footnote_transition = True
+myst_dmath_double_inline = True
